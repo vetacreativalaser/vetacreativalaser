c8d200cec328d1edc2c63197441174e429150cef
IsaacDeMe <isaacdelfamedina@gmail.com>
2025-06-30 19:42:11 +0200

    intento nuevo proyecto

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,17 +1,10 @@
-
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true,
-    storage: localStorage,
-    autoRefreshToken: true,
-  },
-});
-
-if (import.meta.env.DEV) {
-  window.supabase = supabase;
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('❌ Supabase env variables not found');
 }
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
