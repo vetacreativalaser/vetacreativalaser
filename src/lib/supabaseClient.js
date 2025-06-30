497f3ea1e2946d3104befd7c0cd18d9559d24fed
IsaacDeMe <isaacdelfamedina@gmail.com>
2025-06-30 19:01:42 +0200

    supabase forcado

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,13 +1,13 @@
+import { createBrowserClient } from '@supabase/ssr';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error('❌ Supabase env variables not found');
-}
-console.log('URL:', import.meta.env.VITE_SUPABASE_URL);
-console.log('KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
-
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    storage: localStorage,
+    autoRefreshToken: true,
+  },
+});
